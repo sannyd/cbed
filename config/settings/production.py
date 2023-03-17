d01eb59ea83447d4dba378f38ccc231731cfe8ed
Hung Anh Trinh <anh.trinh1@genefriendway.com>
2023-03-17 17:56:56 +0700

    fix email prefix

--- config/settings/production.py
+++ config/settings/production.py
@@ -94,7 +94,7 @@ TEMPLATES[-1]["OPTIONS"]["loaders"] = [  # type: ignore[index] # noqa F405
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
 DEFAULT_FROM_EMAIL = env(
-    "DJANGO_DEFAULT_FROM_EMAIL", default="CBED <noreply@cbed.airdemo.xyz>"
+    "DJANGO_DEFAULT_FROM_EMAIL", default="BED <noreply@api.barexamdrills.com>"
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
