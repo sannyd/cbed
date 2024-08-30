ef13a71fc442fa35a21a1a26c4adebe325f817c8
AnthonyTrinh <hunganh.trinh@totalticketing.com>
2024-08-30 13:56:18 +0700

    CI deploy

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,14 +1,13 @@
-from django.test import TestCase
-
-from cbed.main.models import Level
+import pytest
 
 
-class LevelTestCase(TestCase):
-    def setUp(self):
-        self.level1 = Level.objects.create(name="lv 1")
-        self.level2 = Level.objects.create(name="lv 2")
+@pytest.mark.django_db
+def test_model(self):
+    from cbed.main.models import Level
 
-    def test_animals_can_speak(self):
-        print(self.level1.order)
-        print(self.level2.order)
-        self.assertIsNotNone(self.level1)
+    self.level1 = Level.objects.create(name="lv 1")
+    self.level2 = Level.objects.create(name="lv 2")
+
+    print(self.level1.order)
+    print(self.level2.order)
+    self.assertIsNotNone(self.level1)
